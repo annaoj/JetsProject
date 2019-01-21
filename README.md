3b5fff6f0cd253f1c384a3b3714dd86422e83fac
Anna Jimenez <annajimenez146@gmail.com>
2019-01-20 19:19:01 -0700

    added option to choose jet type

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 ## Jets Application
 
-Description
+Description:
+This project simulates a management application for fleet of jets.A user is given a menu with options to manipulate a fleet of jets with different capabilities.
+There are 5 is loaded initially and user can add 5 more. 
+
+Menu example:
+
+What would you like to do?
+1. List fleet
+2. Fly all jets
+3. View fastest jet
+4. View jet with longest range
+5. Load all Cargo Jets
+6. Dogfight!
+7. Add a jet to Fleet
+8. Quit 
 
 
 ### Technologies/Topics
@@ -13,4 +27,5 @@ Description
 * UML
 
 
-Image, UML download from SD Github page
+UML class diagram : 
+![alt text]( https://raw.githubusercontent.com/SkillDistillery/SD19/master/java1/Jets/images/UMLJets.png?token=AOPN23MxNs2h8zLIlZ-udjmMns4-zu1Bks5cTlx1wA%3D%3D "UML diagram")
--- src/com/skilldistillery/jets/AirField.java
+++ src/com/skilldistillery/jets/AirField.java
@@ -4,107 +4,115 @@ import java.util.Scanner;
 
 public class AirField {
 	Jet[] myJetList = new Jet[10];
-	
+
 	public AirField() {
-		myJetList[0] = new CargoPlane("C-130",368,2361,30_000_000);
-		myJetList[1] = new FighterJet("T-38",858,2300,6_173_000);
-		myJetList[2] = new CargoPlane("KC-135",580,1500,39_600_000);
-		myJetList[3] = new FighterJet("F-22 Raptor",858,2300,6_173_000);
-		myJetList[4] = new FighterJet("F-16",1500,2622,18_800_000);
-		
+		myJetList[0] = new CargoPlane("C-130", 368, 2361, 30_000_000);
+		myJetList[1] = new FighterJet("T-38", 858, 2300, 6_173_000);
+		myJetList[2] = new CargoPlane("KC-135", 580, 1500, 39_600_000);
+		myJetList[3] = new FighterJet("F-22 Raptor", 858, 2300, 6_173_000);
+		myJetList[4] = new FighterJet("F-16", 1500, 2622, 18_800_000);
+
 	};
-	
+
 	public void flyAllJets() {
 		for (Jet jet : myJetList) {
-			if(jet != null) {
+			if (jet != null) {
 				jet.fly();
-				
+
 			}
 		}
 	};
-	
+
 	public void loadAllCargo() {
 		for (Jet jet : myJetList) {
-			if(jet != null && jet instanceof CargoPlane) {
+			if (jet != null && jet instanceof CargoPlane) {
 				CargoPlane temp = (CargoPlane) jet;
-				temp.loadCargo();			
-				
+				temp.loadCargo();
+
 			}
 		}
 	}
-	
+
 	public void listFleet() {
 		int count = 0;
 		for (Jet jet : myJetList) {
-			if(jet != null) {
+			if (jet != null) {
 
-				jet.listInfoJet();		
-				count = count+1;
+				jet.listInfoJet();
+				count = count + 1;
 			}
 		}
 		System.out.println("There are " + count + " planes");
 	}
-	
+
 	public void viewFastestJet() {
 		Jet tempFast = myJetList[0];
 		for (Jet jet : myJetList) {
-			if(jet != null && (tempFast.getSpeed() < jet.getSpeed())) {
+			if (jet != null && (tempFast.getSpeed() < jet.getSpeed())) {
 				tempFast = jet;
 			}
 		}
 		System.out.println("The fastest jet is " + tempFast.getModel() + "\nFull Information: ");
 		tempFast.listInfoJet();
 	}
-	
+
 	public void viewJetWithLongestRange() {
 		Jet tempLong = myJetList[0];
 		for (Jet jet : myJetList) {
-			if(jet != null && (tempLong.getRange() < jet.getRange())) {
+			if (jet != null && (tempLong.getRange() < jet.getRange())) {
 				tempLong = jet;
 			}
 		}
-		System.out.println(tempLong.getModel() +" has the longest range " + "\nFull Information: ");
+		System.out.println(tempLong.getModel() + " has the longest range " + "\nFull Information: ");
 		tempLong.listInfoJet();
 	}
-	
 
 	public void addJet() {
 		String model;
 		int speed;
 		int range;
 		long price;
-		
+		int type;
+
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Is it Fighter Jet (1) or Cargo plane (2) :");
+		type = sc.nextInt();
 		System.out.println("What is the model of your plane? : ");
 		model = sc.next();
-		
+
 		System.out.println("What is the speed of your plane? : ");
 		speed = sc.nextInt();
-		
+
 		System.out.println("What is the range of your plane? : ");
 		range = sc.nextInt();
-		
+
 		System.out.println("What is the price of your plane? : ");
 		price = sc.nextLong();
-		
-		Jet newCustomJet = new JetImpl( model, speed, range, price);
-		
-		for (int i = 0; i < myJetList.length; i++) {
-			if(myJetList[i] == null) {
-				myJetList[i] = newCustomJet;
-				break;
-			}	
+		Jet newCustomJet = null;
+		if (type == 1) {
+			newCustomJet = new FighterJet(model, speed, range, price);
+
+		} else if (type == 2) {
+			newCustomJet = new CargoPlane(model, speed, range, price);
+
+		} else {
+			System.out.println("Wrong input");
 		}
 		
-		
+		for (int i = 0; i < myJetList.length; i++) {
+			if (myJetList[i] == null) {
+				myJetList[i] = newCustomJet;
+				break;
+			}
+		}
+
 	}
-	
-	
+
 	public void makeJetsFight() {
 		for (Jet jet : myJetList) {
-			if(jet != null && jet instanceof FighterJet) {
+			if (jet != null && jet instanceof FighterJet) {
 				FighterJet temp = (FighterJet) jet;
-				temp.fight();						
+				temp.fight();
 			}
 		}
 	}
